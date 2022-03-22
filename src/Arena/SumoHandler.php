a2b6f4e2c33183997beca1f54ce0fceb7825d364
KohakuChanX <notkungz.suphakorn@gmail.com>
2022-03-22 21:14:24 +0700

    Fix Huge Bugs

--- src/Arena/SumoHandler.php
+++ src/Arena/SumoHandler.php
@@ -85,8 +85,7 @@ class SumoHandler implements Listener
         if (!$this->data["enabled"]) {
             $this->plugin->getLogger()->error("Can not load arena: Arena is not enabled!");
             return;
-        }
-        if (!$restart) {
+        } else if (!$restart) {
             $this->plugin->getServer()->getPluginManager()->registerEvents($this, $this->plugin);
             if (!Server::getInstance()->getWorldManager()->isWorldLoaded($this->data["level"])) {
                 Server::getInstance()->getWorldManager()->loadWorld($this->data["level"]);
@@ -115,12 +114,10 @@ class SumoHandler implements Listener
         if (!$this->data["enabled"]) {
             $player->sendMessage(Loader::getPrefixCore() . "§e The game is in configurating!");
             return;
-        }
-        if (count($this->players) >= $this->data["slots"]) {
+        } else if (count($this->players) >= $this->data["slots"]) {
             $player->sendMessage(Loader::getPrefixCore() . "§eThe game is full!");
             return;
-        }
-        if ($this->inGame($player)) {
+        } else if ($this->inGame($player)) {
             $player->sendMessage(Loader::getPrefixCore() . "You are already in the queue/game!");
             return;
         }
@@ -166,7 +163,7 @@ class SumoHandler implements Listener
             $players[$player->getName()] = $player;
         }
         $this->players = $players;
-        $this->phase = self::PHASE_LOBBY;
+        $this->phase = 1;
     }
 
     public function startRestart()
