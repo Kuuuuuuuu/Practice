553f3e7fc9c98ec44ae26c9b5866934c9d570399
KohakuChanX <notkungz.suphakorn@gmail.com>
2022-03-25 15:50:23 +0700

    ไยกมไยกกไดไดไ

--- src/Entity/Leaderboard.php
+++ src/Entity/Leaderboard.php
@@ -10,7 +10,6 @@ use pocketmine\entity\Human;
 use pocketmine\entity\Location;
 use pocketmine\entity\Skin;
 use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\math\Vector3;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataProperties;
 
@@ -25,8 +24,9 @@ class Leaderboard extends Human
         parent::__construct($location, $skin, $nbt);
         $this->skin = new Skin('Standard_Custom', str_repeat("\x00", 8192), '', 'geometry.humanoid.custom');
         $this->sendSkin();
-        $this->setFlying(true);
-        $this->teleport(new Vector3($this->getPosition()->x, 10, $this->getPosition()->z));
+        $this->forceMovementUpdate = false;
+        $this->gravity = 0;
+        $this->setScale(0.1);
         $this->getNetworkProperties()->setFloat(EntityMetadataProperties::BOUNDING_BOX_HEIGHT, 0);
         $this->getNetworkProperties()->setFloat(EntityMetadataProperties::BOUNDING_BOX_HEIGHT, 0);
     }
