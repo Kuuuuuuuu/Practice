676b719d901f9e8a03254fbb85fd5228e53425db
KohakuChanX <notkungz.suphakorn@gmail.com>
2022-04-18 04:56:59 +0700

    Optimize :/

--- src/Task/NeptuneTask.php
+++ src/Task/NeptuneTask.php
@@ -35,9 +35,11 @@ class NeptuneTask extends Task
     {
         if ($this->tick % 20 === 0) {
             Loader::getDeleteBlockHandler()->update();
-            foreach ($this->DuelTask as $duel) {
-                if ($duel instanceof DuelFactory) {
-                    $duel->update();
+            if (count($this->DuelTask) > 0) {
+                foreach ($this->DuelTask as $duel) {
+                    if ($duel instanceof DuelFactory) {
+                        $duel->update();
+                    }
                 }
             }
             if (Loader::getInstance()->Restarted) {
--- src/Utils/DeleteBlocksHandler.php
+++ src/Utils/DeleteBlocksHandler.php
@@ -28,23 +28,22 @@ class DeleteBlocksHandler
 
     public function update(): void
     {
-        if (count($this->buildBlocks) === 0) {
-            return;
-        }
-        foreach ($this->buildBlocks as $pos => $sec) {
-            $block = explode(':', $pos);
-            $x = (int)$block[0];
-            $y = (int)$block[1];
-            $z = (int)$block[2];
-            $level = Server::getInstance()->getWorldManager()->getWorldByName($block[3]);
-            $block = $level->getBlockAt($x, $y, $z);
-            $blockvec = new Vector3($x, $y, $z);
-            if ($sec <= 0) {
-                $level->addSound($blockvec, new BlockPunchSound($block));
-                $level->setBlock($blockvec, VanillaBlocks::AIR());
-                unset($this->buildBlocks[$pos]);
-            } else {
-                $this->buildBlocks[$pos]--;
+        if (count($this->buildBlocks) > 0) {
+            foreach ($this->buildBlocks as $pos => $sec) {
+                $block = explode(':', $pos);
+                $x = (int)$block[0];
+                $y = (int)$block[1];
+                $z = (int)$block[2];
+                $level = Server::getInstance()->getWorldManager()->getWorldByName($block[3]);
+                $block = $level->getBlockAt($x, $y, $z);
+                $blockvec = new Vector3($x, $y, $z);
+                if ($sec <= 0) {
+                    $level->addSound($blockvec, new BlockPunchSound($block));
+                    $level->setBlock($blockvec, VanillaBlocks::AIR());
+                    unset($this->buildBlocks[$pos]);
+                } else {
+                    $this->buildBlocks[$pos]--;
+                }
             }
         }
     }
