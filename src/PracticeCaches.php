fc584ded9bfeacbc35d5b4cde5ecc160c899e259
Nayuki <me@nayukikuu.xyz>
2022-11-11 01:18:57 +0700

    HM

--- src/PracticeCaches.php
+++ src/PracticeCaches.php
@@ -4,13 +4,22 @@ namespace Kuu;
 
 class PracticeCaches
 {
+    /** @var array */
     public array $DuelMatch = [];
+    /** @var array */
     public array $targetPlayer = [];
+    /** @var array */
     public array $KillLeaderboard = [];
+    /** @var array */
     public array $DeathLeaderboard = [];
+    /** @var array */
     public array $ParkourLeaderboard = [];
+    /** @var array */
     public array $ClickData = [];
+    /** @var array */
     public array $buildBlocks = [];
+    /** @var bool */
     public bool $Restarting = false;
+    /** @var array */
     public array $LeapCooldown = [];
 }
