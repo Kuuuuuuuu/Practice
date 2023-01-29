5f0550edb31f8a45aa7db532289015546294f7e3
Kuuuuuuuu <69802296+Kuuuuuuuu@users.noreply.github.com>
2023-01-29 23:11:22 +0700

    Rewrite

--- src/Game/Generator/SumoGenerator.php
+++ src/Game/Generator/SumoGenerator.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Nayuki\Game\Generator;
 
-use pocketmine\block\BlockLegacyIds;
+use pocketmine\block\VanillaBlocks;
 use pocketmine\world\ChunkManager;
 use pocketmine\world\generator\Generator;
 
@@ -13,10 +13,17 @@ final class SumoGenerator extends Generator
     public function generateChunk(ChunkManager $world, int $chunkX, int $chunkZ): void
     {
         $chunk = $world->getChunk($chunkX, $chunkZ);
-        if (($chunk !== null) && $chunkX === 5 && $chunkZ === 5) {
-            for ($x = -10; $x < 12; $x++) {
-                for ($z = 0; $z < 12; $z++) {
-                    $chunk->setFullBlock($x, 100, $z, BlockLegacyIds::GRASS << 4);
+        if (($chunk !== null) && $chunkX % 20 === 0 && $chunkZ % 20 === 0) {
+            for ($x = 0; $x < 16; $x++) {
+                for ($z = 0; $z < 16; $z++) {
+                    if ($x === 0 || $z === 0) {
+                        for ($y = 99; $y < 256; $y++) {
+                            $chunk->setFullBlock($x, $y, $z, VanillaBlocks::GLASS()->getFullId());
+                        }
+                    } else {
+                        $chunk->setFullBlock($x, 100, $z, VanillaBlocks::OAK_PLANKS()->getFullId());
+                        $chunk->setFullBlock($x, 100, $z, VanillaBlocks::OAK_PLANKS()->getFullId());
+                    }
                 }
             }
         }
