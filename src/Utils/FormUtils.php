12482fcf4d99b1f907605d0912add057342181c9
Kuuuuuuuu <69802296+Kuuuuuuuu@users.noreply.github.com>
2023-04-06 23:18:56 +0700

    update

--- src/Utils/FormUtils.php
+++ src/Utils/FormUtils.php
@@ -37,12 +37,6 @@ class FormUtils
 			    case 2:
 				  PracticeCore::getArenaManager()->joinArenas($player, 'Resistance');
 				  break;
-			    case 3:
-				  PracticeCore::getArenaManager()->joinArenas($player, 'Combo');
-				  break;
-			    case 4:
-				  PracticeCore::getArenaManager()->joinArenas($player, 'Build');
-				  break;
 			    default:
 				  print 'Error';
 			 }
@@ -52,9 +46,7 @@ class FormUtils
         $form->addButton("§aNodebuff\n§bPlayers: §f" . PracticeCore::getArenaFactory()->getPlayers(PracticeCore::getArenaFactory()->getArenas('Nodebuff')), 0, 'textures/items/potion_bottle_splash_heal.png');
         $form->addButton("§aFist\n§bPlayers: §f" . PracticeCore::getArenaFactory()->getPlayers(PracticeCore::getArenaFactory()->getArenas('Fist')), 0, 'textures/items/beef_cooked.png');
         $form->addButton("§aResistance\n§bPlayers: §f" . PracticeCore::getArenaFactory()->getPlayers(PracticeCore::getArenaFactory()->getArenas('Resistance')), 0, 'textures/items/snowball.png');
-        $form->addButton("§aCombo\n§bPlayers: §f" . PracticeCore::getArenaFactory()->getPlayers(PracticeCore::getArenaFactory()->getArenas('Combo')), 0, 'textures/items/diamond_sword.png');
-        $form->addButton("§aBuild\n§bPlayers: §f" . PracticeCore::getArenaFactory()->getPlayers(PracticeCore::getArenaFactory()->getArenas('Build')), 0, 'textures/items/diamond_pickaxe.png');
-        $player->sendForm($form);
+	  $player->sendForm($form);
     }
 
     /**
